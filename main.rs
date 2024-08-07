0bfec710fd4b2c98ba61e038bd81f3d5d1de7ea3
Hamza Sajjad <hamza.sajjad@accelercomm.com>
2024-08-07 16:54:15 +0100

    init

---
+++ main.rs
@@ -0,0 +1,38 @@
+
+
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5; 
+
+    println!("{}",sum);
+
+    get_volt()
+
+    // Recusive formula for find the mean
+
+    // alpha = (k-1) / k
+    // x_bar[k] = (alpha * x_bar[k-1]) + ((1 - alpha)*x_bar[k])
+}
+
+fn get_volt(){
+    let rand = 11.1111111212;
+    let w = 4.0*rand; 
+    let z = 14.4 + w;
+    println!("{}", z);
+
+} 
+
+
